64c8c5b8d5a08112622323bb2e0274387e5634ca
AllegorickBlock <jason.bourlet@live.com>
2018-03-05 09:12:50 +0100

    Site web

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = mysqli_connect('localhost','root','','tfe');
-
-mysqli_query($mysqli,"INSERT INTO exemple(nom,prenom,age) VALUES ('Poutou','Philippe','40')") or die(mysqli_error($mysqli));
-
-mysqli_close($mysqli);
-
-
-?>
